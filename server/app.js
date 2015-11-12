0e97f84abd8cfafa79d45bfcedcfe72841f942d0
Gregg Swoverland <swoverla@hotmail.com>
2015-11-12 15:16:07 -0500

    Send json web token in response

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
 require('dotenv').load();
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.use(require('./middleware/headers'));
 
 app.use('/api/v1/notes', require('./routes/notes'));
+app.use('/api/v1/users', require('./routes/users'));
 
 app.listen(3000, function() {
-  console.log('Listening on http:localhost:3000');
+  console.log('Listening on http://localhost:3000');
 });
